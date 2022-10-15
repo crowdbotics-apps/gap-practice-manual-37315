ed8c740501dfe7490aae925a630feabeb322f6e1
Crowdbotics <team@crowdbotics.com>
2022-10-15 22:28:36 +0000

    Initial commit.

---
+++ backend/gap_practice_manual_37315/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gap_practice_manual_37315 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gap_practice_manual_37315.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gap_practice_manual_37315",
+  url: "https://gap_practice_manual_37315.botics.co",
+  api: "https://gap_practice_manual_37315.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
